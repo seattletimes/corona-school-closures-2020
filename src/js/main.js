cd1e1dd5c53bd8f753bd2c8cec042815791342e7
llflannery <lflannery@seattletimes.com>
2020-03-09 17:55:21 -0700

    closer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // var paywall = require("./lib/paywall");
 // setTimeout(() => paywall(12345678), 5000);
-var schools = require("../../data/public.sheet.json");
-
+var pub_schools = require("../../data/public.sheet.json");
+var pri_schools = require("../../data/private.sheet.json");
 require("component-responsive-frame/child");
 
 var today = new Date();
@@ -11,42 +11,84 @@ var m = String(today.getMonth() + 1).padStart(1, '0'); //January is 0!
 
 // today = m + '/' + dd + '/' + yyyy;
 today = m + '/' + dd;
-console.log(today);
+console.log(pri_schools);
+
+function publicSchools() {
+  pub_schools.forEach((element) => {
+    var thisSchool = document.getElementById( element.id );
+    if ( element.endDate >= today ){
+      document.getElementById( element.county ).classList.remove("hideMe");
+    } else {
+      thisSchool.style.display = "none";
+    }
+  });
+};
 
 
-schools.forEach((element) => {
+function privateSchools() {
+  pri_schools.forEach((element) => {
+    var thisSchool = document.getElementById( element.id );
+    if ( element.endDate >= today ){
+      console.log(element.endDate);
+      document.getElementById( element.county ).classList.remove("hideMe");
+    } else {
+      thisSchool.style.display = "none";
+    }
+  });
+};
 
-  var thisSchool = document.getElementById( element.id );
-
-  if ( element.endDate >= today ){
-
-  } else {
-    thisSchool.style.display = "none";
-  }
 
 
-});
 
+
+
+var containers = document.getElementsByClassName("container");
+
+
+for (var i = 0; i < containers.length; i++) {
+
+var privateORpublic = containers[i].getAttribute("id");
+
+console.log(privateORpublic);
+
+if ( privateORpublic === "private" ){
+  privateSchools();
+} else { publicSchools(); }
 
 
 var filterCounties = document.getElementsByClassName("countyButton");
 var allCards = document.querySelectorAll("[data-county]");
 
 var myFunction = function() {
+
+    for (var i = 0; i < filterCounties.length; i++) {
+        filterCounties[i].classList.remove("selected");
+    }
+
+    this.classList.add("selected");
     var countyID = this.getAttribute("id");
     var matchedCounty = document.querySelectorAll(`[data-county='${ countyID }']`);
 
-    for (var i = 0; i < allCards.length; i++) {
-        allCards[i].classList.add("hideMe");
-    }
+    if ( countyID === "all" ){
+      for (var i = 0; i < allCards.length; i++) {
+          allCards[i].classList.remove("hideMe");
+      }
+    } else {
+      for (var i = 0; i < allCards.length; i++) {
+          allCards[i].classList.add("hideMe");
+      }
 
-    for (var i = 0; i < matchedCounty.length; i++) {
-        matchedCounty[i].classList.remove("hideMe");
+      for (var i = 0; i < matchedCounty.length; i++) {
+          matchedCounty[i].classList.remove("hideMe");
+      }
     }
 
 
 };
 
+//add Click event to all county filter buttons
 for (var i = 0; i < filterCounties.length; i++) {
     filterCounties[i].addEventListener('click', myFunction, false);
+}
+
 }
